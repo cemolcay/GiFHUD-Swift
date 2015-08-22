055e9125dc2a55e25f37e8aa5f16fecc04388c2d
Goktug Yilmaz <goktugyil@gmail.com>
2015-08-22 16:20:27 +0300

    Added showForSeconds method inside
    I only needed to use it this way

--- GiFHUD-Swift/GiFHUD.swift
+++ GiFHUD-Swift/GiFHUD.swift
@@ -346,6 +346,14 @@ class GiFHUD: UIView {
             self.instance.fadeIn()
         })
     }
+
+    class func showForSeconds (seconds: Double) {
+        show()
+        let time = dispatch_time(DISPATCH_TIME_NOW, Int64(seconds * Double(NSEC_PER_SEC)))
+        dispatch_after(time, dispatch_get_main_queue(), {
+            GiFHUD.dismiss()
+        })
+    }
     
     class func dismiss () {
         if (!self.instance.shown) {
